3b2460cad9e4a7479b802425a307733e86b87807
Kristi Kuhnen <kkuhnen@gala.games>
2023-01-12 20:21:49 -0800

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Testing',
+
+  projectId: 'o2h9758v',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
